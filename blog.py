7db284f92beb4757126bb75395d28590720e637b
Ndelle Herbert <ndelleh04@gmail.com>
2025-09-09 20:25:40 +0100

    Improve blog service functions: fix update with exclude_unset, return structured response for delete, and ensure session commits correctly

--- blog.py
+++ blog.py
@@ -1,38 +1,35 @@
-import model
-import schema
-from sqlalchemy.orm import session
+import model,schema
+from sqlalchemy.orm import Session
 
 
-def create_blog(db:session, my_blog:schema.create_blog):
-    blog = model.Blog(**my_blog.model_dump())
+def create_blog(db:Session, my_blog:schema.CreateBlog):
+    blogs = model.Blog(**my_blog.model_dump())
 
-    db.add(blog)
+    db.add(blogs)
     db.commit()
-    db.refresh(blog)
-    return blog
+    db.refresh(blogs)
+    return blogs
 
-def get_all_blogs(db:session):
-    blog = db.query(model.Blog).all()
-    return blog
+def get_all_blog(db:Session):
+    blogs = db.query(model.Blog).all()
+    return blogs
 
-def get_blog(db:session,id:int):
-    blog = db.query(model.Blog).filter(model.Blog.id == id).first()
+def get_blog(db:Session,id:int):
+    blogs = db.query(model.Blog).filter(model.Blog.user_id == id).first()
+    return blogs
 
-    return blog
-
-def update_blog(id:int,my_blog:schema.update_blog,db:session):
-    blog = get_blog(db,id)
-    if blog:
-        for key, value in my_blog.model_dump().items:
-            setattr(blog,key,value)
-
+def update_blog(id:int,my_blog:schema.UpdateBlog,db:Session):
+    blogs = get_blog(db,id)
+    if blogs:
+        for key, value in my_blog.model_dump().items():
+            setattr(blogs,key,value)
+            db.add(blogs)
             db.commit()
-            db.refresh(blog)
-    return blog
+            db.refresh(blogs)
+    return blogs
 
-def delete_blog(id:int, db:session):
-    blog = get_blog(db,id)
-    db.delete(blog)
+def delete_blog(id:int, db:Session):
+    blogs = get_blog(db,id)
+    db.delete(blogs)
     db.commit()
-    db.refresh(blog)
     return "Blog deleted"
